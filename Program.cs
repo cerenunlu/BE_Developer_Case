afda72bfc521cad502277fe1b11dd604d22b9d1a
cerenunlu <75950978+cerenunlu@users.noreply.github.com>
2022-05-29 18:03:33 +0300

    Case3

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+// using Newtonsoft;
+// using Newtonsoft.Json;
+using System.Text.Json;
+using System.Collections.Generic;
+namespace KaizenJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ReadFile();
+        }
+
+         public static void ReadFile(){
+         string dosya_yolu=@"C:\Users\MSI PC\Desktop\response.json.rtf";
+         String st=File.ReadAllText(dosya_yolu);
+        
+       var list = JsonSerializer.Deserialize<List<Test>>(st);
+      foreach (var item in list)
+      {
+          Console.WriteLine(item.description);
+         
+      }
+      
+        }
+    }
+    public class Test{
+        public string locale {get;set;}
+        public string description {get;set;}
+       
+    }
+}
